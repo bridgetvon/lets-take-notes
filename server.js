f931886590f5be5e82100ed1d8589c3311f58c81
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-30 16:24:18 -0700

    moving styles paths

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 // set up a path name look for an api route
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
